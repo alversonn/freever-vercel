1d75ceecc7183f9da55606bade6c396004228ba5
alversonn <alifabdulhakim.ah@gmail.com>
2025-09-28 19:33:22 +0700

    fix: use bcryptjs in all api routes

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 // src/app/api/register/route.ts
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
